2fdec3d903cac23a4e40d4134db52f2996955c0a
Tetiana Zubko <zubko.tetiana@pdffiller.team>
2017-09-26 14:22:18 +0300

    first commit

---
+++ Console/Commands/NewCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace mypackage\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Tests\Input\InputArgumentTest;
+
+class NewCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'newcommand';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'It is a new command';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info("Done!");
+    }
+}
---
+++ src/NewCommandServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace mypackage;
+
+use Illuminate\Support\ServiceProvider;
+use mypackage\Console\Commands\NewCommand;
+
+class NewCommandServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                NewCommand::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
